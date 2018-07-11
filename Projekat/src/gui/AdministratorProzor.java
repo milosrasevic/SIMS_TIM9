aceda149abb89ffa3317144c217a4ea641686aa0
Unknown <ljubicjanko1@gmail.com>
2018-07-11 02:33:21 +0200

    Dodata veza mesta stanice i centrale sa korisnicima

--- Projekat/src/gui/AdministratorProzor.java
+++ Projekat/src/gui/AdministratorProzor.java
@@ -25,6 +25,7 @@ public class AdministratorProzor extends Stage {
 		this.setScene(new Scene(root,1000,500));
 		this.setMaximized(true);
 		this.initStyle(StageStyle.UTILITY);
+		this.setTitle("ADMINISTRATOR");
 		
 		root.setTop(hbox1);
 		hbox1.setStyle("-fx-background-color: #336699;");
