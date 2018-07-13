c5542ff49171ffeb5ee4814acdb75d5d75f071c2
Unknown <ljubicjanko1@gmail.com>
2018-07-13 13:15:00 +0200

    Popravka radnik centrale i admin

--- Projekat/src/gui/AdministratorProzor.java
+++ Projekat/src/gui/AdministratorProzor.java
@@ -3,8 +3,8 @@ package gui;
 
 
 import java.io.File;
+import java.util.ArrayList;
 
-import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -12,6 +12,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -19,13 +20,19 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import model.Centrala;
+import model.Deonica;
 
 public class AdministratorProzor extends Stage implements EventHandler<ActionEvent>{
 
+	private Centrala centrala = Centrala.getInstance();
+	
 	// DUGMAD OSNOVNOG PROZORA
 	private Button btn_odjava = new Button("Odjavi se");
-	private Button btn_IzvestajiStanice = new Button("IZVESTAJI NAPLATNIH STANICA");
-	private Button btn_IzvestajiMesta = new Button("IZVESTAJI NAPLATNIH MESTA");
+	private Button btn_rad_sa_korisnicima = new Button("IZMENA NA KORSINICIMA");
+	private Button btn_rad_sa_deonicama = new Button("IZMENA NA DEONICAMA");
+	private Button btn_rad_sa_stanicama = new Button("IZMENA NA STANICAMA");
+	private Button btn_rad_sa_cenovnicina = new Button("UNOS CENOVNIKA");
 	
 	private HBox zaglavlje = new HBox();
 	private HBox ikonice = new HBox(50);
@@ -34,26 +41,15 @@ public class AdministratorProzor extends Stage implements EventHandler<ActionEve
 	
 	
 	// DUGMAD PROZORA ZA IZVESTAJE STANICA
-	private Button btn_broj_vozila = new Button("Broj vozila");
-	private Button btn_naplaceni_iznos = new Button("Naplaceni iznos");
-	private Button btn_kvarovi = new Button("Kvarovi");
-	private Button btn_broj_vozila_period = new Button("Broj vozila period");
+	
 	private Button btn_nazad = new Button("Nazad");
-	private ComboBox lista_stanica = new ComboBox<>();
-	private Label tekst_zaglavlja_stanice = new Label("  STANICA");
-	private HBox zaglavlje_stanica = new HBox(150);
-	private HBox ikonice_stanice = new HBox(120);
-	private HBox dugmad_stanice = new HBox(120);
-	private HBox podglavlje_stanica = new HBox();
-	private VBox vbox_stanice = new VBox();
+	
 	
 	private BorderPane osnovni_layout = new BorderPane();
-	private BorderPane stanice_layout = new BorderPane();
-	private BorderPane mesta_layout = new BorderPane();
+	
 	
 	private Scene osnovnaScena = new Scene(osnovni_layout,1000,500);
-	private Scene izvestajiStanicaScena = new Scene(stanice_layout,1000,500);
-	private Scene izvestajiMestaScena = new Scene(mesta_layout,1000,500);
+	
 	
 	public AdministratorProzor()
 	{
@@ -72,21 +68,19 @@ public class AdministratorProzor extends Stage implements EventHandler<ActionEve
 		zaglavlje.setPadding(new Insets(10,10,10,0));
 
 		
-		Image image = new Image(new File("src/slike/izvestaji.png").toURI().toString());
-		ImageView iv = new ImageView();
-		iv.setImage(image);
-		ikonice.getChildren().add(iv);
-		ikonice.setAlignment(Pos.CENTER);
-		vbox1.getChildren().add(ikonice);
 		
 		
-		btn_IzvestajiStanice.setStyle("-fx-font: 18 arial; -fx-base: #b6e7c9;");
-		btn_IzvestajiStanice.setPrefSize(400, 50);
-		btn_IzvestajiMesta.setStyle("-fx-font: 18 arial; -fx-base: #b6e7c9;");
-		btn_IzvestajiMesta.setPrefSize(400, 50);
+		btn_rad_sa_deonicama.setStyle("-fx-font: 18 arial; -fx-base: #b6e7c9;");
+		btn_rad_sa_cenovnicina.setStyle("-fx-font: 18 arial; -fx-base: #b6e7c9;");
+		btn_rad_sa_stanicama.setStyle("-fx-font: 18 arial; -fx-base: #b6e7c9;");
+		btn_rad_sa_korisnicima.setStyle("-fx-font: 18 arial; -fx-base: #b6e7c9;");
+		
+		btn_rad_sa_deonicama.setPrefSize(400, 500);
+		btn_rad_sa_cenovnicina.setPrefSize(400, 500);
+		btn_rad_sa_korisnicima.setPrefSize(400, 500);
+		btn_rad_sa_stanicama.setPrefSize(400, 500);
 
-		dugmad.getChildren().add(btn_IzvestajiMesta);
-		dugmad.getChildren().add(btn_IzvestajiStanice);
+		dugmad.getChildren().addAll(btn_rad_sa_korisnicima, btn_rad_sa_deonicama, btn_rad_sa_stanicama, btn_rad_sa_cenovnicina);
 		dugmad.setAlignment(Pos.CENTER);
 		vbox1.getChildren().add(dugmad);
 		
@@ -94,54 +88,12 @@ public class AdministratorProzor extends Stage implements EventHandler<ActionEve
 		vbox1.setAlignment(Pos.CENTER_RIGHT);
 		
 		
-		// SCENA ZA IZVESTAJE STANICA
-		//****************************************************************************
-		tekst_zaglavlja_stanice.setStyle("-fx-font: 50 arial; -fx-base: #b6e7c9;");
-		lista_stanica.setStyle("-fx-font: 30 arial; -fx-base: #b6e7c9;");
-		zaglavlje_stanica.setAlignment(Pos.CENTER);
-		zaglavlje_stanica.setStyle("-fx-background-color: #336699;");
-		zaglavlje_stanica.getChildren().addAll(tekst_zaglavlja_stanice, lista_stanica);
-		
-		ImageView[] imageView = new ImageView[3];
-		
-		imageView[0] = new ImageView(); imageView[0].setImage(new Image(new File("src/slike/automobil.jpg").toURI().toString()));
-		imageView[1] = new ImageView(); imageView[1].setImage(new Image(new File("src/slike/kes.png").toURI().toString()));
-		imageView[2] = new ImageView(); imageView[2].setImage(new Image(new File("src/slike/kvar.png").toURI().toString()));
-		for(int i = 0; i < 3; i++) {
-			imageView[i].setFitHeight(100);
-			imageView[i].setFitWidth(100);
-		}
-		ikonice_stanice.getChildren().addAll(imageView[0], imageView[1], imageView[2]);
-		ikonice_stanice.setAlignment(Pos.CENTER);
-		vbox_stanice.getChildren().add(ikonice_stanice);
-		
-		
-		btn_broj_vozila.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");	btn_naplaceni_iznos.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");
-		btn_kvarovi.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");	btn_broj_vozila_period.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");
-		dugmad_stanice.getChildren().addAll(btn_broj_vozila, btn_naplaceni_iznos, btn_kvarovi, btn_broj_vozila_period);
-		dugmad_stanice.setAlignment(Pos.CENTER);
-		vbox_stanice.getChildren().add(dugmad_stanice);
-		
-		podglavlje_stanica.setAlignment(Pos.BOTTOM_CENTER);
-		btn_nazad.setStyle("-fx-font: 100 arial; -fx-base: #b6e7c9;");
-		podglavlje_stanica.getChildren().add(btn_nazad);
-		
-		
-		stanice_layout.setCenter(vbox_stanice);
-		stanice_layout.setTop(zaglavlje_stanica);
-		stanice_layout.setBottom(podglavlje_stanica);
-		
-		
-		
-		
-		btn_IzvestajiStanice.setOnAction(this);
-		btn_IzvestajiMesta.setOnAction(this);
+		btn_rad_sa_korisnicima.setOnAction(this);
+		btn_rad_sa_deonicama.setOnAction(this);
 		btn_odjava.setOnAction(this);
-		btn_broj_vozila.setOnAction(this);
-		btn_naplaceni_iznos.setOnAction(this);
-		btn_kvarovi.setOnAction(this);
-		btn_broj_vozila_period.setOnAction(this);
-		btn_nazad.setOnAction(this);
+		btn_rad_sa_stanicama.setOnAction(this);
+		btn_rad_sa_cenovnicina.setOnAction(this);
+
 		this.showAndWait();
 		
 	}
@@ -149,23 +101,16 @@ public class AdministratorProzor extends Stage implements EventHandler<ActionEve
 	public void handle(ActionEvent event) {
 		if(event.getSource() == btn_odjava) {
 			this.close();
-		}else if(event.getSource() == btn_IzvestajiStanice) {
-			this.setScene(izvestajiStanicaScena);
+		}else if(event.getSource() == btn_rad_sa_korisnicima) {
+			
 			obradiVelicinu();
-		}else if(event.getSource() == btn_IzvestajiMesta) {
-			this.setScene(izvestajiMestaScena);
+		}else if(event.getSource() == btn_rad_sa_deonicama) {
+			
 			obradiVelicinu();
-		}else if(event.getSource() == btn_broj_vozila) {
+		}else if(event.getSource() == btn_rad_sa_stanicama) {
 			
-		}else if(event.getSource() == btn_naplaceni_iznos) {
+		}else if(event.getSource() == btn_rad_sa_cenovnicina) {
 			
-		}else if(event.getSource() == btn_kvarovi) {
-			
-		}else if(event.getSource() == btn_broj_vozila_period) {
-			
-		}else if(event.getSource() == btn_nazad) {
-			this.setScene(osnovnaScena);
-			obradiVelicinu();
 		}
 	}
 	public void obradiVelicinu() {
